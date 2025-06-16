fe03a3f005017cefcb5dd6f9328f84c8f4f9fb2f
off777 <megovipa@gmail.com>
2025-06-16 21:04:21 +0200

    playwright

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import requests
+from playwright.sync_api import sync_playwright
+
+
 def main():
-    print("test wiadomosc")
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto("https://example.com")
+        page.wait_for_load_state("domcontentloaded")
+        print(page.inner_html("body"))
+        browser.close()
 
 
 if __name__ == "__main__":
