1c4f5ffb1ea2bb49b41281d8bf0e37c98e6d3e3b
off777 <megovipa@gmail.com>
2025-06-16 21:14:58 +0200

    olx

--- main.py
+++ main.py
@@ -8,26 +8,21 @@ from selenium.webdriver.support import expected_conditions as EC
 def main():
     chrome_options = ChromeOptions()
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920,1080")
-    # chrome_options.add_argument("--no-sandbox") # Often needed in CI/CD environments
-    # chrome_options.add_argument("--disable-dev-shm-usage") # Often needed in CI/CD environments
+    # chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=300,300")
 
     driver = webdriver.Chrome(options=chrome_options)
 
     try:
-        # Navigate to the URL
-        driver.get("https://example.com")
+        driver.get("https://www.olx.pl/motoryzacja/q-samochody-osobowe/")
 
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.TAG_NAME, "body"))
         )
 
-        # Print the inner HTML of the body
         print(driver.find_element(By.TAG_NAME, "body").get_attribute("innerHTML").strip())
 
     finally:
-        # Close the browser
         driver.quit()
 
 
