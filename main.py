a0e3d748954e4a44cc40b490cbc30f86cb5a5cd0
off777 <megovipa@gmail.com>
2025-06-16 21:21:49 +0200

    soup

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
+from bs4 import BeautifulSoup
 
+link = 'https://www.olx.pl/motoryzacja/q-samochody-osobowe/'
 
-def main():
-    chrome_options = ChromeOptions()
-    chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=300,300")
+text = requests.get(link).text
 
-    driver = webdriver.Chrome(options=chrome_options)
+soup = BeautifulSoup(text, 'html.parser')
 
-    try:
-        driver.get("https://www.olx.pl/motoryzacja/q-samochody-osobowe/")
-
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, "body"))
-        )
-
-        print(driver.find_element(By.TAG_NAME, "body").get_attribute("innerHTML").strip())
-
-    finally:
-        driver.quit()
-
-
-if __name__ == "__main__":
-    main()
+print(soup)
